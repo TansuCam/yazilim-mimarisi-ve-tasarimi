083fa83ea1f72acfc1689bb25878eb4b32170c95
TansuCam <59123506+TansuCam@users.noreply.github.com>
2019-12-22 23:48:35 +0300

    Add files via upload

---
+++ FactoryMethodTasarimDeseni/Creator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoryMethod
+{
+    enum ReaderModel
+    {
+        Word,
+        PDF,
+        Txt
+    }
+    class Creator
+    {
+        public Reader ReaderFactory(ReaderModel readerModel)
+        {
+            Reader reader = null;
+            switch (readerModel)
+            {
+                case ReaderModel.Word:
+                    reader = new Word();
+                    break;
+                case ReaderModel.PDF:
+                    reader = new PDF();
+                    break;
+                case ReaderModel.Txt:
+                    reader = new Txt();
+                    break;
+                default:
+                    break;
+                    
+            }
+            return reader;
+        }
+    }
+}
---
+++ FactoryMethodTasarimDeseni/Reader.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace FactoryMethod
+{
+    public abstract class Reader
+    {
+        public abstract void Read();
+    }
+
+    public class PDF : Reader
+    {
+        public override void Read()
+        {
+            Console.WriteLine("PDF Dosyası..");
+        }
+    }
+
+    public class Word : Reader
+    {
+        public override void Read()
+        {
+            Console.WriteLine("Word Dosyası..");
+        }
+    }
+
+    public class Txt : Reader
+    {
+        public override void Read()
+        {
+            Console.WriteLine("Txt Dosyası..");
+        }
+    }
+}
