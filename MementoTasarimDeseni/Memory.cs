e75407bc6c0b22ba24ef096530c2edb908eea84a
TansuCam <59123506+TansuCam@users.noreply.github.com>
2019-12-22 23:43:47 +0300

    Create Memory.cs

---
+++ MementoTasarimDeseni/Memory.cs
@@ -0,0 +1,13 @@
+```
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MementoPattern
+{
+    class Memory
+    {
+        public Memento ProductMemento { get; set; }
+    } 
+}
+```
